009325cee7174bcf1e9040bf4da998f0163b6d12
Barri <info@bdesignstudio.co.uk>
2020-07-08 18:24:51 +0100

    Style the side bar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import style from './header.module.css';
 import { Link } from 'react-router-dom';
+import SideNavigation from './SideNav/SideNav';
 
 import FontAwesome from 'react-fontawesome';
 
-export const Header = () => {
+export const Header = (props) => {
 
   const logo =()=>(
       <Link to="/" className={style.logo}>
@@ -13,7 +14,8 @@ export const Header = () => {
     )
     const navBars = () => (
       <div className={style.bars}>
-        <FontAwesome name="bars"
+                <FontAwesome name="bars"
+                onClick={props.onOpenNav}
           style={{
             color:'#dfdfdf',
             padding: '10px',
@@ -25,6 +27,7 @@ export const Header = () => {
 
   return (
     <header className={style.header}>
+      <SideNavigation {...props}/>
      <div className ={style.headerOpt}>
      {navBars()}
      {logo()}
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -4,14 +4,23 @@ import './layout.css';
 import moduleName, { Header } from '../../components/Header/Header';
 export class Layout extends Component {
   state = {
-
+    showNav:false
   }
-
+ 
+  toggleSideNav= (action) => {
+    this.setState({
+      showNav:action
+    })
+  }
   render() {
     return (
       <div>
         
-       <Header/>
+       <Header
+        showNav={this.state.showNav}
+        onHideNav={()=> this.toggleSideNav(false)}
+       onOpenNav={() => this.toggleSideNav(true)}
+       />
         {this.props.children}
         footer
       </div>
