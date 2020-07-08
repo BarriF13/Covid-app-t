159f79926e537a3e66b2a2df604b136fdbf82cff
Barri <info@bdesignstudio.co.uk>
2020-07-08 17:58:37 +0100

    Add Logo and navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,20 +2,33 @@ import React from 'react'
 import style from './header.module.css';
 import { Link } from 'react-router-dom';
 
+import FontAwesome from 'react-fontawesome';
 
 export const Header = () => {
 
-  const logo =()=>{
-    return (
+  const logo =()=>(
       <Link to="/" className={style.logo}>
         <img src="/img/logo.jpg"  alt="COVID Logo"/>
       </Link>
     )
-  }
+    const navBars = () => (
+      <div className={style.bars}>
+        <FontAwesome name="bars"
+          style={{
+            color:'#dfdfdf',
+            padding: '10px',
+            pointer:'curser'
+          }}
+        />
+      </div>
+    )
+
   return (
     <header className={style.header}>
-     <div className ={style.headerOpt}></div>
+     <div className ={style.headerOpt}>
+     {navBars()}
      {logo()}
+     </div>
     </header>
   )
 }
