0a66bd78f216d237502f5ffc7a4e411bb03e317e
Barri <info@bdesignstudio.co.uk>
2020-07-09 12:14:23 +0100

    Add load more function

--- src/components/widgets/NewsList/NewsList.js
+++ src/components/widgets/NewsList/NewsList.js
@@ -1,39 +1,75 @@
 import React, { Component } from 'react';
-import {CSSTransition, TransitionGroup} from 'react-transition-group';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import { Link } from 'react-router-dom';
 import axios from 'axios'
 
-import {URL} from '../../../config';
-
+import { URL } from '../../../config';
+import style from './newsList.module.css'
 
 
 export class NewsList extends Component {
 
-  state ={
-    items:[],
-    start:this.props.start,
-    end:this.props.start + this.props.amount,
-    amount:this.props.amount
+  state = {
+    items: [],
+    start: this.props.start,
+    end: this.props.start + this.props.amount,
+    amount: this.props.amount
   }
 
   //check request
 
-  componentWillMount(){
-    //request
-    axios.get(`${URL}/articles?_start=${this.state.start}&_end${this.state.end}`)
-    //catching the promise
-    .then(res => {
-      this.setState({
-       items:[...this.state.items, ...res.data]
-      })
-    })
-
+  componentWillMount() {
+    this.request(this.state.start,this.state.end)
   }
+  request = (start, end) => {
+    //request
+    axios.get(`${URL}/articles?_start=${start}&_end=${end}`)
+      //catching the promise
+      .then(res => {
+        this.setState({
+          items:[...this.state.items, ...res.data]
+        })
+      })
+  }
+  loadMore = ()=>{
+let end = this.state.end + this.state.amount
+
+    this.request(this.state.end, end)
+  }
+
+  renderNews = (type) => {
+    let template = null;
+
+    switch (type) {
+      case ('card'):
+        template = this.state.items.map((item, i) => (
+          <div>
+            <div className={style.newsList_item}>
+              <Link to={`/articles/${item.id}`}>
+                <h2>{item.title}</h2>
+              </Link>
+
+            </div>
+          </div>
+
+        ))
+        break;
+      default:
+        template = null;
+    }
+
+    return template;
+  }
+
+
   render() {
     console.log(this.state.items);
     return (
       <div>
-        hello news list
+        {this.renderNews(this.props.type)}
+        <div onClick={()=> this.loadMore()}>
+          Load More
+        </div>
       </div>
     )
   }
