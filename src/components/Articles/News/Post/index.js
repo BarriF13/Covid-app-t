b93cfcb9bcce6836b687dcc16cd591bbf11f9528
Barri <info@bdesignstudio.co.uk>
2020-07-18 16:04:16 +0100

    Add post html

--- src/components/Articles/News/Post/index.js
+++ src/components/Articles/News/Post/index.js
@@ -1,30 +1,32 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
 // import { URL } from '../../../../config'
-import {firebaseLooper, firebaseHospitals, firebaseDB} from '../../../../firebase'
+import { firebase, firebaseLooper, firebaseHospitals, firebaseDB } from '../../../../firebase'
 import style from '../../article.module.css';
 import Header from './Header';
 export class NewArticles extends Component {
   state = {
     article: [],
-    hospital: []
+    hospital: [],
+    imageURL: ''
 
   }
   componentWillMount() {
     firebaseDB.ref(`articles/${this.props.match.params.id}`).once('value')
-    .then((snapshot)=>{
-      let article = snapshot.val();
+      .then((snapshot) => {
+        let article = snapshot.val();
 
-      firebaseHospitals.orderByChild('hospitalId').equalTo(article.hospital).once('value')
-      .then((snapshot)=>{
-        const hospital = firebaseLooper(snapshot)
-        this.setState({
-          article,
-          hospital
-        })
+        firebaseHospitals.orderByChild('hospitalId').equalTo(article.hospital).once('value')
+          .then((snapshot) => {
+            const hospital = firebaseLooper(snapshot)
+            this.setState({
+              article,
+              hospital
+            })
+            this.getImageURL(article.image)
+          })
+
       })
-
-    })
 
 
 
@@ -42,28 +44,45 @@ export class NewArticles extends Component {
     //       })
     //   })
   }
+  //function for catching a name of image
+  getImageURL= (filename)=>{
+    firebase.storage().ref('images')
+    .child(filename).getDownloadURL()
+    .then( url =>{
+      this.setState({
+        imageURL:url
+      })
+    })
+  }
+
   render() {
     const article = this.state.article;
     const hospital = this.state.hospital;
     // console.log(this.state);
     return (
       <div className={style.articleWrapper}>
-          <Header
-            hospitalData={hospital[0]}
-            date={article.date}
-            author={article.author}
-          />
-          <div className={style.articleBody}>
-            <h1>{article.title}</h1>
-              <div className={style.articleImage}
-                    style={{
-                    background: `url('/images/articles/${article.image}')`
-                    }}
+        <Header
+          hospitalData={hospital[0]}
+          date={article.date}
+          author={article.author}
+        />
+        <div className={style.articleBody}>
+          <h1>{article.title}</h1>
+          <div className={style.articleImage}
+            style={{
+              // background: `url('/images/articles/${article.image}')`
+              background: `url('${this.state.imageURL}')`
+            }}
           ></div>
-            <div className={style.articleText}>
-              {article.body}
-            </div>
+          <div className={style.articleText}
+          dangerouslySetInnerHTML={{
+            __html:article.body
+
+          }}             
+          >
+            {/* {article.body} */}
           </div>
+        </div>
       </div>
     )
   }
